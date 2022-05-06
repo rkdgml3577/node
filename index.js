b80f5783b3820710b337478d82d4402a685c62c9
rkdgml3577 <101076374+rkdgml3577@users.noreply.github.com>
2022-05-06 23:46:33 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://CHOIKANGHEE:choi0730!A@node.cu7l8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕하세요')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
