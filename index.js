01d817b4c7db561113cc2338518694c4d9ddd8f8
rkdgml3577 <101076374+rkdgml3577@users.noreply.github.com>
2022-05-07 14:48:44 +0900

    ADD: 회원가입

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { user } =require("./models/user");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://CHOIKANGHEE:choi0730!A@node.cu7l8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
 
 }).then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err))
+  .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World! 안녕하세요')
-})
+
+  app.get('/', (req, res) => {res.send('Hello World! 안녕하세요')})
+
+
+  app.post('/register', (req, res) => {
+    
+    //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다
+     
+
+
+    const User = new user(req.body)
+
+    User.save((err, doc) => {
+      if (err) return res.json({success: false, err})
+      return res.status(200).json({
+        success: true
+      })
+    })
+  })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
